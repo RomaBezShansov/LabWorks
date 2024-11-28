171097151829a517a284ddf93416f2dddb881548
RomaBezShansov <RomaCujba@yandex.ru>
2024-11-28 11:12:57 +0200

    28/11

---
+++ Lab 8/HeapInt.h
@@ -0,0 +1,28 @@
+#pragma once // 1
+#include <cassert>
+
+class HeapInt
+{
+    int* heapValue; // 2
+
+public:
+    // 3
+    inline HeapInt(int val) : heapValue(new int{ val }){}
+
+    inline ~HeapInt()
+    {
+        // Заметка: delete проверяет на nullptr сам.
+        delete heapValue; // 5
+    }
+
+    inline int& asRef()
+    {
+        assert(heapValue != nullptr); // 6
+        return *heapValue;
+    }
+
+    HeapInt(const HeapInt& hello); // 7
+    HeapInt(HeapInt&& hello);
+    void operator=(const HeapInt& hello);
+    void operator=(HeapInt&& hello);
+};
